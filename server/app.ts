fae45cb93c7a9f4c5bcc1a35de880e2e87340d82
abdulkader <abdulkader_hassan74@hotmail.com>
2022-04-08 02:03:53 +0200

    initial app, server and routes config plus configuring tsconfig.json

---
+++ server/app.ts
@@ -0,0 +1,25 @@
+require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import { allRoutes } from './routes';
+export class App {
+  private app: express.Application;
+  constructor() {
+    this.app = express();
+    this.initializeMiddlewares();
+  }
+  private initializeMiddlewares() {
+    this.app.use(express.json({ limit: '10mb' }));
+    this.app.use(cors());
+    this.app.use(allRoutes);
+    //todo: winston logger byb error handlerMiddleWare
+  }
+  public async listen(PORT?: number) {
+    const port = PORT ? PORT : +process.env.PORT;
+    this.app.listen(port, process.env.HOST, () => {
+      console.log(
+        `Server is listening on port ${port}, running on environment ${process.env.NODE_ENV}`
+      );
+    });
+  }
+}
